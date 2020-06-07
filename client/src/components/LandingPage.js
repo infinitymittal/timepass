8d3da7852ccd66b86ea65508efb0df50ee11f802
Anant Mittal <infinitymittal@gmail.com>
2020-06-07 00:19:54 -0700

    Lifted name and gameId up.

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import {PageEnum} from './Page.js';
+
 export default class LandingPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			name: '',
-			gameId: 'GAME',
+			gameId: '',
 		};
 		
 		this.handleNameChange = this.handleNameChange.bind(this);
@@ -22,8 +24,13 @@ export default class LandingPage extends React.Component {
 	}
 
 	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.name);
+		//Make proper post. Get gameId if needed. Change Page.
+		if(this.state.gameId)
+			alert(this.state.name+' is joining new game ' + this.state.gameId);
+		else
+			alert(this.state.name+' is starting a new game ');
 		event.preventDefault();
+		this.props.onNewGame(this.state.gameId, this.state.name);
 	}
 
 	
@@ -32,14 +39,14 @@ export default class LandingPage extends React.Component {
 		return (
 			<div className='Page'>
 				<div>{message}</div>
+				<hr/>
 				<div>
 					<form onSubmit={this.handleSubmit}>
-						<label>Name:</label>
-						<input type="text" value={this.state.name} onChange={this.handleNameChange} />
+						<input type="text" placeholder="Enter Your Name" value={this.state.name} onChange={this.handleNameChange} />
 						<hr/>
 						<label>
-							Join Game:
-							<input type="text" value={this.state.gameId} onChange={this.handleGameIdChange} />
+							Join Existing Game? 
+							<input type="text" placeholder="Enter Game Id" value={this.state.gameId} onChange={this.handleGameIdChange} />
 						</label>
 						<hr/>
 						<input type="submit" value="Start"/>
--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -2,17 +2,27 @@ import React from 'react';
 
 import LandingPage from './LandingPage.js';
 
-const PageEnum = Object.freeze({'Landing':1, 'Waiting':2, 'Game':3,});
+export const PageEnum = Object.freeze({'Landing':'Landing', 'Waiting':'Waiting', 'Playing':'Playing',});
 export default class Page extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			page:PageEnum.Landing,
+			gameId:'',
+			name:'',
 		};
 	}
 	
+	startGame(gameId, name) {
+		this.setState({
+			page:PageEnum.Waiting,
+			gameId:gameId,
+			name:name,
+		});
+	}
+	
 	render() {
-		return <LandingPage />;
+		return <LandingPage onNewGame={(id, name)=>this.startGame(id, name)}/>;
 	}
 }
 
