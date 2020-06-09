e53aca4133338a17064e27053f0090d9ddd51ab6
Anant Mittal <infinitymittal@gmail.com>
2020-06-08 17:36:26 -0700

    Adding phase change checker

--- client/src/components/PlayingPage.js
+++ client/src/components/PlayingPage.js
@@ -8,16 +8,20 @@ export const PhaseEnum = Object.freeze({
 	'Night':'Sleeping',
 });
 
+const AllowedEnum = Object.freeze({'Yes':'Yes', 'No':'No', 'Checking':'Checking'});
+
 export default class PlayingPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			players:props.players,
 			gameId:props.gameId,
-			phase:PhaseEnum.Discuss1
+			phase:PhaseEnum.Discuss1,
+			canChangePhase:AllowedEnum.No,
 		};
 		this.handleGetRole = this.handleGetRole.bind(this);
 		this.handlePhaseChange = this.handlePhaseChange.bind(this);
+		this.getCanPhaseChange = this.getCanPhaseChange.bind(this);
 	}
 	
 	handleGetRole() {
@@ -29,6 +33,7 @@ export default class PlayingPage extends React.Component {
 	handlePhaseChange() {
 		this.setState({
 			phase:this.getNextPhase(),
+			canChangePhase:AllowedEnum.No,
 		});
 	}
 	
@@ -38,7 +43,41 @@ export default class PlayingPage extends React.Component {
 			case PhaseEnum.Vote1: return PhaseEnum.Discuss2;
 			case PhaseEnum.Discuss2: return PhaseEnum.Vote2;
 			case PhaseEnum.Vote2: return PhaseEnum.Night;
+			default:
 			case PhaseEnum.Night: return PhaseEnum.Discuss1;
+		}
+	}
+	
+	getCanPhaseChange() {
+		const allowed = Math.floor(Math.random()*2)===0; //TODO get from server
+		if(allowed)
+			this.setState({
+				canChangePhase:AllowedEnum.Yes,
+			});
+		else {
+			this.setState({
+				canChangePhase:AllowedEnum.Checking,
+			});
+			setTimeout(this.getCanPhaseChange, 5000);
+		}
+	}
+
+	getPhaseActions() {
+		switch(this.state.canChangePhase) {
+			default:
+			case AllowedEnum.Yes:
+				const nextPhase = this.getNextPhase();
+				const buttonText = "Start "+nextPhase;
+				const phaseActions = (
+					<button onClick={()=>this.handlePhaseChange()}>{buttonText}</button>
+				);
+				return phaseActions;
+			case AllowedEnum.No:
+				setTimeout(this.getCanPhaseChange, 1000);
+				//fall through
+			case AllowedEnum.Checking:
+				const message = "Waiting to be allowed phase change.";
+				return (<div>{message}</div>);
 		}
 	}
 	
@@ -49,11 +88,7 @@ export default class PlayingPage extends React.Component {
 		});
 		const getRoleText = "See my role.";
 		const phaseMessage = "It is "+this.state.phase+" time.";
-		const nextPhase = this.getNextPhase();
-		const buttonText = "Start "+nextPhase;
-		const phaseActions = (
-			<button onClick={()=>this.handlePhaseChange()}>{buttonText}</button>
-		);
+		const phaseActions = this.getPhaseActions();
 		
 		return (
 			<div className='Page'>
@@ -62,6 +97,7 @@ export default class PlayingPage extends React.Component {
 				<ul>{playerList}</ul>
 				<button onClick={()=>this.handleGetRole()}>{getRoleText}</button>
 				<div>{phaseMessage}</div>
+				<hr/>
 				{phaseActions}
 			</div>
 		);
