07b06035a63d38371c55e64057e380ba8dee45a2
Anant Mittal <infinitymittal@gmail.com>
2020-06-09 11:28:09 -0700

    Allow all CORS

--- server/src/main/java/com/example/springboot/PlayerController.java
+++ server/src/main/java/com/example/springboot/PlayerController.java
@@ -1,13 +1,14 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class PlayerController {
 
-	@CrossOrigin(origins = "http://localhost:3000")
+	@CrossOrigin(origins = CorsConfiguration.ALL)
 	@RequestMapping("/game/{gameId}/player/{playerId}/role")
 	public Role getRole() {
 		return new Role();
--- server/src/main/java/com/example/springboot/WelcomeController.java
+++ server/src/main/java/com/example/springboot/WelcomeController.java
@@ -1,13 +1,14 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class WelcomeController {
 
-	@CrossOrigin(origins = "http://localhost:3000")
+	@CrossOrigin(origins = CorsConfiguration.ALL)
 	@RequestMapping("/")
 	public String index() {
 		return "Welcome to Mafia server.";
